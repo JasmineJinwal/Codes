cbdfdd6a0b08914d2583500a57f8874f0cfdfd21
Jasmine Jinwal <41662107+JasmineJinwal@users.noreply.github.com>
2018-09-18 17:47:40 +0530

    Add files via upload

--- CLOCK_Gen.vhd
+++ CLOCK_Gen.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,19 +32,42 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity CLOCK_Gen is
-    Port ( CLK : out STD_LOGIC);
+    Port ( CLK1 , CLK2, CLK3 : out STD_LOGIC);
 end CLOCK_Gen;
 
 architecture Behavioral of CLOCK_Gen is
 
 begin
 
-process
+process                        -- process statement 1
 begin
-    clk <= '0';
+for i in 1 to 5 loop           -- clock 1 of time period  10 nano seconds
+    CLK1 <= '0';
     wait for 10 NS;
-    clk <= '1';
+    CLK1 <= '1';
     wait for 10 NS;
-end process;
+end loop;
+end process;                   -- end of process statement 1
+
+process                       -- process statement 2
+begin
+for i in 1 to 5 loop          -- clock 2 of time period  5 nano seconds
+        CLK2 <= '0';
+        wait for 5 NS;
+        CLK2 <= '1';
+        wait for 5 NS;
+end loop;
+end process;                   -- end of process statement 2
+
+process                      -- process statement 3
+begin       
+for i in 1 to 5 loop          -- clock 3 of time period  2 nano seconds
+            CLK3 <= '0';
+            wait for 2 NS;
+            CLK3 <= '1';
+            wait for 2 NS;
+end loop;
+end process;                   -- end of process statement 3
 
 end Behavioral;
+
